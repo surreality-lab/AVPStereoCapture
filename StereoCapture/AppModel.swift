524aa9cc7c9460a71b39f86c6d2d9617c9b243f3
Griffin Hurt <griffhurt@pitt.edu>
2025-06-30 13:40:01 -0400

    Initial Commit

---
+++ StereoCapture/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  StereoCapture
+//
+//  Created by Griffin Hurt on 6/30/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ StereoCapture/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  StereoCapture
+//
+//  Created by Griffin Hurt on 6/30/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
